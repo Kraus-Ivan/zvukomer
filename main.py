cf42a305bc23df4332be9374202eea7b50e1c8c3
Ivan Kraus <91247726+Kraus-Ivan@users.noreply.github.com>
2021-11-13 17:20:39 +0100

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+#ULOZENI NAHODNEHO CASU TONU
+delka_tonu = 0
+def btnB():
+    global delka_tonu
+    delka_tonu = randint(1000, 10000)
+    music.play_tone(frequency = 262, ms = delka_tonu)
+    print(delka_tonu)
+input.on_button_pressed(Button.B, btnB)
+
+#OPAKOVANE PREHRATI ULOZENEHO TONU
+def btnA():
+    global delka_tonu
+    if delka_tonu > 0:
+        music.play_tone(frequency = 262, ms = delka_tonu)
+input.on_button_pressed(Button.A, btnA)
+
+#MERENI CASU UZIVATELE
 stav_mereni = False
 pocatecni_cas = 0
 cas_mereni = 0
@@ -12,10 +29,27 @@ def ukonceni_mereni():
     global pocatecni_cas, stav_mereni, cas_mereni
     konecny_cas = control.millis()
     if stav_mereni == True:
-        cas_mereni = Math.round((konecny_cas - pocatecni_cas)/1000)
+        cas_mereni = (konecny_cas - pocatecni_cas)
         stav_mereni = False
+    print(cas_mereni)
+    vyhodnoceni()
 input.on_logo_event(TouchButtonEvent.RELEASED, ukonceni_mereni)
 
-def test():
-    print(cas_mereni)
-input.on_button_pressed(Button.A, test)
+#VYHODNOCENI
+#POKUD cas_mereni JE VETSI JAK delka_tonu, ZOBRAZI SE ">" (ODCHYLKA 0,5 SEKUNDY)
+def vyhodnoceni():
+    global cas_mereni, delka_tonu
+    if cas_mereni <= (delka_tonu + 500):
+        led.plot_bar_graph(cas_mereni, delka_tonu)
+        if led.point(0, 0):
+            soundExpression.happy.play()
+    else:
+        basic.show_leds("""
+        . # . . .
+        . . # . .
+        . . . # .
+        . . # . .
+        . # . . .
+        """)
+        soundExpression.sad.play()
+
--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
+// ULOZENI NAHODNEHO CASU TONU
+let delka_tonu = 0
+input.onButtonPressed(Button.B, function btnB() {
+    
+    delka_tonu = randint(1000, 10000)
+    music.playTone(262, delka_tonu)
+    console.log(delka_tonu)
+})
+// OPAKOVANE PREHRATI ULOZENEHO TONU
+input.onButtonPressed(Button.A, function btnA() {
+    
+    if (delka_tonu > 0) {
+        music.playTone(262, delka_tonu)
+    }
+    
+})
+// MERENI CASU UZIVATELE
 let stav_mereni = false
 let pocatecni_cas = 0
 let cas_mereni = 0
@@ -10,11 +27,33 @@ input.onLogoEvent(TouchButtonEvent.Released, function ukonceni_mereni() {
     
     let konecny_cas = control.millis()
     if (stav_mereni == true) {
-        cas_mereni = Math.round((konecny_cas - pocatecni_cas) / 1000)
+        cas_mereni = konecny_cas - pocatecni_cas
         stav_mereni = false
     }
     
-})
-input.onButtonPressed(Button.A, function test() {
     console.log(cas_mereni)
+    vyhodnoceni()
 })
+// VYHODNOCENI
+// POKUD cas_mereni JE VETSI JAK delka_tonu, ZOBRAZI SE ">" (ODCHYLKA 0,5 SEKUNDY)
+function vyhodnoceni() {
+    
+    if (cas_mereni <= delka_tonu + 500) {
+        led.plotBarGraph(cas_mereni, delka_tonu)
+        if (led.point(0, 0)) {
+            soundExpression.happy.play()
+        }
+        
+    } else {
+        basic.showLeds(`
+        . # . . .
+        . . # . .
+        . . . # .
+        . . # . .
+        . # . . .
+        `)
+        soundExpression.sad.play()
+    }
+    
+}
+
